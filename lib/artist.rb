055345ddc8ac4371963062fdecaff201f6c2fedc
Eric Loudon <loudon.eric@gmail.com>
2019-08-26 11:42:34 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,31 +1,34 @@
+require 'pry'
+
 class Artist
+attr_accessor :name, :songs
 
-   attr_accessor :name
+@@song_count = 0
 
- @count = 1
-
-   def initialize(name)
-    @name = name
-    @songs = []
-  end
-
-   def add_song(song)
-    @songs << song
-    song.artist = self
-  end
-
-   def songs
-    @songs
-  end
-
-   def add_song_by_name(name)
-    song = Song.new(name)
-    @songs << song
-  song.artist = self
-  end
-
- def self.song_count
- @count += 1
+def initialize(name) #allows access and stores artist name
+  @name = name
+  @songs = []
 end
 
- end
+def add_song(song)
+self.songs << song #creates new instances of artist & adds it to the artist array of songs
+song.artist = self
+@@song_count += 1
+end
+
+def add_song_by_name(name)
+song = Song.new(name) #takes in an arguement for a song and then associates it with the song and artist
+self.songs << song
+song.artist = self
+@@song_count += 1
+end
+
+def songs
+@songs
+end
+
+def self.song_count
+@@song_count
+end
+
+end
