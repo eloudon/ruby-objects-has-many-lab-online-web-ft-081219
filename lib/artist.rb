610f39d260ac2fbcb79297f8b139fab5218a7033
Eric Loudon <loudon.eric@gmail.com>
2019-08-26 11:36:39 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,31 +1,23 @@
 class Artist
   attr_accessor :name
 
-  @@song_count = 0
-
   def initialize(name)
-    @name = name
     @songs = []
-  end
-
-  def add_song(song)
-    self.songs << song
-    song.artist = self
-    @@song_count +=1
-  end
-
-  def add_song_by_name(name)
-    song = Song.new(name)
-    self.songs << song
-    song.artist = self
-    @@song_count +=1
+    @name = name
   end
 
   def songs
     @songs
   end
 
-  def self.song_count
-    @@song_count
+  def add_song(song)
+    @songs << song
+    song.artist = self
+  end
+
+  def genres
+    self.songs.collect do |song|
+      song.genre
+    end
   end
 end
